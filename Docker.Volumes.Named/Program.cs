a63be8340a5df0bb5b819f59bb82abc3e45c2170
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-12-14 00:55:28 +0700

    Add project files.

---
+++ Docker.Volumes.Named/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Docker.Volumes.Named;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var str = "Docker from scratch!";
+        Console.WriteLine(str);
+
+        Console.ReadLine();
+    }
+}
+
