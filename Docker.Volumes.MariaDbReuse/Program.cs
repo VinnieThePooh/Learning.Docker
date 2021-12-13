dcaaed0a4e7e2c98ad83084e3984f99d28117ce1
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-12-14 01:08:46 +0700

    feat(Docker): 2 docker use case projects added

---
+++ Docker.Volumes.MariaDbReuse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Docker.Volumes.MariaDbReuse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
