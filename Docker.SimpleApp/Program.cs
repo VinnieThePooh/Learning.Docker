cc2230a79a783e10567a6b72e274aa5bdb79a77e
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-12-18 23:20:21 +0700

    Docker.SimpleApp ,todos added

---
+++ Docker.SimpleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Docker.SimpleApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("[.NET App within Docker]: Hello World!");
+        Console.ReadLine();
+        Console.WriteLine("[.NET App within Docker]: Application running completed");
+    }
+}
