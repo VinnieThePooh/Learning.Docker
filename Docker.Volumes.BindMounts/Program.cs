52f1cc78b0692ec4cc73647822603188302b5988
Lashkevich Ryan <lashkevichrain@gmail.com>
2021-12-19 19:09:58 +0700

    feat(Docker): volumes with bind mounts app added

--- Docker.Volumes.BindMounts/Program.cs
+++ Docker.Volumes.BindMounts/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Docker.Volumes.BindMounts
 {
@@ -6,7 +7,48 @@ namespace Docker.Volumes.BindMounts
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var filePath = Path.Combine("VolumeData", "data.txt");
+
+            if (!File.Exists(filePath))
+            {
+                PrintMessage("data.txt doesn't exist..skipping");
+                PrintMessage("App running completed successfully.");
+                return;
+            }
+
+            var messages = File.ReadAllLines(filePath);
+
+            PrintMessage("Messages: ");
+            foreach (var message in messages)
+                PrintMessage(message, ConsoleColor.DarkYellow);
+
+
+            // add new message mode
+            if (args.Length == 1)
+            {
+                var newMessage = args[0];
+                File.AppendAllLines(filePath, new []{newMessage});
+                PrintMessage($"Added new message: \"{newMessage}\"");
+            }
+            else
+                PrintMessage($"No new message provided..skipped adding");
+
+            PrintMessage("App running completed successfully.");
+        }
+
+        static void PrintMessage(string message, ConsoleColor? color = null)
+        {
+            var prefix = "Docker bind mounts demo";
+            if (color is null)
+            {
+                Console.WriteLine($"[{prefix}]: {message}");
+                return;
+            }
+
+            Console.Write($"[{prefix}]: ");
+            Console.ForegroundColor = color.Value;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
