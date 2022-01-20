1615573f50b2bb68f8ddfeaf241215415755482f
Lashkevich Ryan <lashkevichrain@gmail.com>
2022-01-20 12:48:30 +0700

    perf(MariaDbReuse.WebApi): Task.WithTimeout - Task.Delay cancelling added

--- Docker.Volumes.MariaDbReuse.WebApi/Extensions/TaskExtensions.cs
+++ Docker.Volumes.MariaDbReuse.WebApi/Extensions/TaskExtensions.cs
@@ -4,9 +4,16 @@ public static class TaskExtensions
 {
     public static async Task WithTimeout(this Task task, TimeSpan timeout)
     {
-        var delay = Task.Delay(timeout);
-        var result = await Task.WhenAny(task, delay);
-        if (result == delay)
-            throw new TimeoutException($"Thrown by operation timeout: {timeout}");        
+        using (var cts = new CancellationTokenSource())
+        {
+            var delay = Task.Delay(timeout, cts.Token);
+            var result = await Task.WhenAny(task, delay);
+            if (result == delay)
+                throw new TimeoutException($"Thrown by operation timeout: {timeout}");
+            else
+            {
+                cts.Cancel();
+            }
+        }         
     }
 }
